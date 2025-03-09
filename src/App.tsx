0ed4aa4d5e47e51024c9bd3fe23506ee8500e205
mariamksalama <mariam_salama97@hotmail.com>
2025-03-10 00:41:06 +0200

    Initial commit: Calculator app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <Typography variant="h1">Your calculator</Typography>
+    </>
+  );
+}
+
+export default App;
